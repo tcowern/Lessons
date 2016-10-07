6f704672a7ba3dc2293090c50f8943c9117f8880
tcowern <tcowern@thomas-cowerns-macbook-pro.local>
2016-10-07 14:11:06 -0600

    Function Practice 2 commit

--- FunctionPractice2/FunctionPractice2.js
+++ FunctionPractice2/FunctionPractice2.js
@@ -29,10 +29,69 @@ function negativeIndex(negArray, num) {
 }
 
 function removeM (str) {
-    for(var i = 0; i < str.length; i++) {
-      var myLetter = str.charAt(i)
-        if (myLetter === m) {
-          str.slice(i,i);
-        } 
+     var answer = str.replace(/m/gi, '');
+     console.log(answer);
+     return answer;
+}
+
+function printObject (myObject){
+
+  var object = "";  
+  for(object in myObject){
+        console.log(object + " is " + myObject[object]);
+
     }
+}
+
+function vowels(myStr){
+  var myArray = [];
+  for (var i =0; i < myStr.length; i++) {
+    if (myStr[i] === 'a' || myStr[i] === 'e' || myStr[i] === 'i' || myStr[i] === 'o' || myStr[i] === 'u') {
+      myArray.push(myStr[i]);
+    }
+  }
+  console.log(myArray);
+  return myArray;
+}
+
+function twins(myArr){
+  var pairs = "";
+  var notpairs = "";
+  var paircheck
+  for (var i = 0; i < myArr.length; i++) {
+    if (myArr[i] === myArr[i + 1]) {
+      pairs++;
+    } else {
+      notpairs++;
+    }
+    
+    if(pairs === notpairs) {
+      paircheck = true;
+    } else {
+      paircheck = false; 
+    }
+  }
+  console.log(paircheck);
+  return paircheck;
+}
+
+function or (myArr) {
+  var truth = "";
+  if (myArr.length === 0) {
+    console.log("false");
+    return false;
+  }
+  for (var i = 0; i < myArr.length; i++) {
+    if (myArr[i] === true) {
+        console.log("True event");
+        truth = true;
+        break;
+    } else {
+        console.log("False event");
+        truth = false;
+    }
+
+  }
+        console.log("truth is " + truth);
+        return truth;
 }
